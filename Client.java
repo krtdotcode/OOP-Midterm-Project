5c0d653dd46b931640575adee9ed0b4d9dd8816b
Kurt Joshua C. <136403509+krtdotcode@users.noreply.github.com>
2025-03-26 03:28:44 +0800

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,50 +1,65 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Client extends Person {
-   private int clientId;
-   private static int count = 1;
-   private int registrationYear;
-   private List<Transaction> transactions = new ArrayList<>();
+	private int clientId;
+	private static int count = 1;
+	private int registrationYear;
+	private List<Transaction> transactions = new ArrayList<>();
+	private boolean isArchived; // kaye
 
+	// Constructor
+	public Client(String name, int contactNumber, String address, int registrationYear) {
+		super(name, contactNumber, address);
+		this.clientId = count++;
+		this.registrationYear = registrationYear;
+	}
 
-   // Constructor
-   public Client(String name, int contactNumber, String address, int 
-                   registrationYear) {
-       super(name, contactNumber, address);
-       this.clientId = count++;
-       this.registrationYear = registrationYear;
-   }
+	// Getters
+	public int getClientId() {
+		return clientId;
+	}
 
-
-   // Getters
-   public int getRegistrationYear() {
+	public int getRegistrationYear() {
 		return registrationYear;
-   }
-	
-   public List<Transaction> getTransactions() {
-       return transactions;
-   }
-  
-   // Add a transaction to the client's list
-   public void addTransaction(Transaction transaction) {
-       if (!transactions.contains(transaction)) {
-           transactions.add(transaction);
-       }
-   }
-   // Display client details
-   @Override
-   public void displayDetails() {
-       super.displayDetails();
-       System.out.println("Client ID: " + registrationYear + "-" +               
-                              String.format("%04d", clientId));
-       System.out.println("Transactions:");
+	}
 
+	public List<Transaction> getTransactions() {
+		return transactions;
+	}
 
-       for (Transaction transaction : transactions) {
-             System.out.println("- Transaction ID: " + 
-                               transaction.getTransactionId());
-       }
-   }
+	// Update details - kaye
+	public void updateDetails(String newName, int newContactNumber, String newAddress) {
+		setName(newName);
+		setContactNumber(newContactNumber);
+		setAddress(newAddress);
+	}
+
+	// Archive methods - kaye
+	public void archive() {
+		isArchived = true;
+	}
+
+	public void unarchive() {
+		isArchived = false;
+	}
+
+	// Add a transaction to the client's list
+	public void addTransaction(Transaction transaction) {
+		if (!transactions.contains(transaction)) {
+			transactions.add(transaction);
+		}
+	}
+
+	// Display client details
+	@Override
+	public void displayDetails() {
+		super.displayDetails();
+		System.out.println("Client ID: " + registrationYear + "-" + String.format("%04d", clientId));
+		System.out.println("Transactions:");
+		for (Transaction transaction : transactions) {
+			System.out.println("- Transaction ID: " + transaction.getTransactionId());
+		}
+	}
+
 }
